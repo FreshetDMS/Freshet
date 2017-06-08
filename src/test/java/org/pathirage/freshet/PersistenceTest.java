70bb7c28ce0a900949875edfb5c9ab258f6b055a
Milinda Pathirage <milinda.pathirage@gmail.com>
2017-06-08 15:51:53 -0400

    more tests for orm layer

--- src/test/java/org/pathirage/freshet/PersistenceTest.java
+++ src/test/java/org/pathirage/freshet/PersistenceTest.java
@@ -15,25 +15,28 @@
  */
 package org.pathirage.freshet;
 
+import io.ebean.Ebean;
 import org.apache.samza.serializers.StringSerdeFactory;
-import org.junit.Assert;
 import org.junit.Test;
+import org.pathirage.freshet.domain.Job;
 import org.pathirage.freshet.domain.StorageSystem;
 import org.pathirage.freshet.domain.Stream;
 import org.pathirage.freshet.domain.Topology;
+
+import static org.junit.Assert.*;
 
 public class PersistenceTest {
   @Test
   public void testTopologyCreation() {
     StorageSystem system = new StorageSystem();
-    system.setIdentifier("kafka");
+    system.setIdentifier("kafka1");
     system.setBrokers("localhost:9092");
     system.setZk("localhost:2181");
 
     system.save();
 
     Stream source = new Stream();
-    source.setIdentifier("source");
+    source.setIdentifier("source1");
     source.setKeySerdeFactory(StringSerdeFactory.class.getName());
     source.setValueSerdeFactory(StringSerdeFactory.class.getName());
     source.setPartitionCount(10);
@@ -44,7 +47,7 @@ public class PersistenceTest {
     system.update();
 
     Stream sink = new Stream();
-    sink.setIdentifier("sink");
+    sink.setIdentifier("sink1");
     sink.setKeySerdeFactory(StringSerdeFactory.class.getName());
     sink.setValueSerdeFactory(StringSerdeFactory.class.getName());
     sink.setPartitionCount(10);
@@ -55,12 +58,93 @@ public class PersistenceTest {
     system.update();
 
     Topology t = new Topology();
-    t.setName("test-topology");
+    t.setName("test-topology1");
     t.addSink(sink);
     t.addSource(source);
 
     t.save();
 
-    Assert.assertNotNull(t.getId());
+    assertNotNull(t.getId());
+
+    Ebean.find(Topology.class)
+        .where().eq("name", "test-topology1")
+        .findOneOrEmpty()
+        .ifPresent(it -> {
+          assertEquals(it.getId(), t.getId());
+          assertEquals(it.getName(), t.getName());
+        });
+  }
+
+  @Test
+  public void testTopologyWithJobs() {
+    StorageSystem system = new StorageSystem();
+    system.setIdentifier("kafka2");
+    system.setBrokers("localhost:9092");
+    system.setZk("localhost:2181");
+
+    system.save();
+
+    Stream source = new Stream();
+    source.setIdentifier("source2");
+    source.setKeySerdeFactory(StringSerdeFactory.class.getName());
+    source.setValueSerdeFactory(StringSerdeFactory.class.getName());
+    source.setPartitionCount(10);
+    source.setSystem(system);
+    source.save();
+
+    system.addStream(source);
+    system.update();
+
+    Stream sink = new Stream();
+    sink.setIdentifier("sink2");
+    sink.setKeySerdeFactory(StringSerdeFactory.class.getName());
+    sink.setValueSerdeFactory(StringSerdeFactory.class.getName());
+    sink.setPartitionCount(10);
+    sink.setSystem(system);
+    sink.save();
+
+    system.addStream(sink);
+    system.update();
+
+    Stream intermediate = new Stream();
+    intermediate.setIdentifier("intermediate1");
+    intermediate.setKeySerdeFactory(StringSerdeFactory.class.getName());
+    intermediate.setValueSerdeFactory(StringSerdeFactory.class.getName());
+    intermediate.setPartitionCount(10);
+    intermediate.setSystem(system);
+    intermediate.save();
+
+    system.addStream(intermediate);
+    system.update();
+
+    Topology t = new Topology();
+    t.setName("test-topology2");
+    t.addSink(sink);
+    t.addSource(source);
+
+    t.save();
+
+    Job job1 = new Job();
+    job1.setIdentifier("job21");
+    job1.addInput(source);
+    job1.addOutput(intermediate);
+    job1.setTopology(t);
+    job1.save();
+
+    Job job2 = new Job();
+    job2.setIdentifier("job22");
+    job2.addInput(intermediate);
+    job2.addOutput(sink);
+    job2.setTopology(t);
+    job2.save();
+
+    t.addJob(job1);
+    t.addJob(job2);
+
+    Ebean.find(Job.class)
+        .where().eq("topology", t)
+        .findEach(it -> {
+          assertTrue(it.getIdentifier().equals("job21") || it.getIdentifier().equals("job22"));
+        });
   }
 }
